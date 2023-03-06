907e4c16c0c234ba71ee4ec0b2ccdf294af89768
László Dao <56759208+N1Official@users.noreply.github.com>
2023-03-06 08:58:30 +0100

    alap

---
+++ MateFinder/MateFinder/Game.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MateFinder
+{
+    class Game
+    {
+        private int _id;
+        private string _name;
+        private string[] _category;
+        private string[] _ranks;
+        private string _current_rank;
+        private int _level;
+        private string _goal;
+
+        public int id { get => _id; set => _id = value; }
+        public string name { get => _name; set => _name = value; }
+        public string[] category { get => _category; set => _category = value; }
+        public string[] ranks { get => _ranks; set => _ranks = value; }
+        public string current_rank { get => _current_rank; set => _current_rank = value; }
+        public int level { get => _level; set => _level = value; }
+        public string goal { get => _goal; set => _goal = value; }
+    }
+}
---
+++ MateFinder/MateFinder/Server.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MateFinder
+{
+    class Server
+    {
+        private int _id;
+        private string _name;
+        private User[] _members;
+        private string _game_category;
+        private int[] _games_id;
+        private string _country;
+        private string _state;
+    }
+}
