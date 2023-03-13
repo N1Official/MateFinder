2e3fd7fc7fead86f8d4ce5fb80a7cb714a77a018
N1Official <dao.laszlo03@gmail.com>
2023-03-13 09:02:50 +0100

    főoldal

--- MateFinder/MateFinder/MainWindow.xaml.cs
+++ MateFinder/MateFinder/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace MateFinder
         {
             InitializeComponent();
         }
+
+        private void exit_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
     }
 }
